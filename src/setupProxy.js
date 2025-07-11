733b2e0de734bfe5f71caeda03c05863b61c990d
Eyob Temesgen <111111244+EyobTemesgen@users.noreply.github.com>
2025-07-11 16:06:51 +0300

    fixed CORS issues

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -11,6 +11,13 @@ module.exports = function(app) {
         // Log the request being proxied
         console.log(`Proxying request: ${req.method} ${req.path} -> ${proxyReq.getHeader('host')}${req.path}`);
       },
+      onProxyRes: function(proxyRes, req, res) {
+        // Add CORS headers
+        proxyRes.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
+        proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+        proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+        proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
+      },
       onError: (err, req, res) => {
         console.error('Proxy error:', err);
         res.status(500).json({ error: 'Proxy error', details: err.message });
